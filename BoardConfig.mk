1c03420f0692f041d4cacb21f24975fc0530b1d8
Gilang EA. <144137514+excaliburXD@users.noreply.github.com>
2026-04-20 20:43:16 +0700

    Rework: WIP!

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,20 +1,10 @@
 #
 # Copyright (C) 2020 The Android Open Source Project
 # Copyright (C) 2026 The TWRP Open Source Project
-# Copyright (C) 2025 The OrangeFox Recovery Project
+# Copyright (C) 2026 The OrangeFox Recovery Project
 # Copyright (C) 2020 SebaUbuntu's TWRP device tree generatort
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 DEVICE_PATH := device/infinix/X695C
@@ -75,7 +65,6 @@ BOARD_DTB_OFFSET := 0x0bc08000
 BOARD_KERNEL_IMAGE_NAME := kernel
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
-TARGET_KERNEL_VERSION := 4.14
 TARGET_FORCE_PREBUILT_KERNEL := true
 
 # MKBOOTIMG-ARGS
@@ -196,7 +185,7 @@ TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 # Storage
 TW_HAS_MTP := true
 TW_MTP_DEVICE := /dev/mtp_usb
-TW_NO_USB_STORAGE := false
+TW_NO_USB_STORAGE := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
 
 # Debug
@@ -216,8 +205,6 @@ TW_INCLUDE_RESETPROP := true
 # Exclude
 TW_EXCLUDE_APEX := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_EXCLUDE_TWRPAPP := true
-TW_EXCLUDE_LPDUMP := true
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_Infinix-X695C
--- device.mk
+++ device.mk
@@ -1,20 +1,10 @@
 #
 # Copyright (C) 2020 The Android Open Source Project
 # Copyright (C) 2026 The TWRP Open Source Project
-# Copyright (C) 2025 The OrangeFox Recovery Project
+# Copyright (C) 2026 The OrangeFox Recovery Project
 # Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
@@ -62,11 +52,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := true
 
-# Crypto
-PRODUCT_ENFORCE_VINTF_MANIFEST :=
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.crypto.volume.filenames_mode=aes-256-cts
-
 # API
 PRODUCT_SHIPPING_API_LEVEL := 28
 BOARD_VNDK_VERSION := current
@@ -74,7 +59,8 @@ BOARD_VNDK_VERSION := current
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-mtkimpl \
-    android.hardware.boot@1.1-mtkimpl.recovery
+    android.hardware.boot@1.1-mtkimpl.recovery \
+    android.hardware.boot@1.1-mtkimpl-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
@@ -94,10 +80,6 @@ PRODUCT_PACKAGES += \
     mtk_plpath_utils \
     mtk_plpath_utils.recovery
 
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
@@ -109,13 +91,11 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Additional Configs
 TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hardware.keymaster@4.0 \
     libkeymaster4 \
     libpuresoftkeymasterdevice \
     libhardware_legacy
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.0 \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libhardware_legacy.so
