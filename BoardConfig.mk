1131b8e0c7d8742f9b4f93117a582f4e5fb30607
Gilang EA. <144137514+excaliburXD@users.noreply.github.com>
2026-07-09 12:51:11 +0700

    prebuilt-12: set default language en id

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,6 +165,7 @@ TW_CUSTOM_BATTERY_POS := "790"
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_LANGUAGES := en id
 TW_DEFAULT_LANGUAGE := en
 TW_EXTRA_LANGUAGES := false
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
@@ -207,7 +208,7 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
 # FS & Tools
-TW_INCLUDE_NTFS_3G := true
+TW_INCLUDE_NTFS_3G := false
 TW_INCLUDE_REPACKTOOLS := true
 TW_INCLUDE_RESETPROP := true
 
